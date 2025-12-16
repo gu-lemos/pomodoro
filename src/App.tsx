99315f9728b4f79b717d4e876f01eae5d51b8ec8
Gustavo Lemos <contato.gustavolemos@outlook.com>
2025-12-15 23:23:30 -0300

    feat: troca de tema usando useEffect e useState

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { Input } from "./components/Input";
-
-import "./styles/theme.css";
-import "./styles/global.css";
 import { Cycles } from "./components/Cycles";
 import { Button } from "./components/Button";
 import { PlayCircleIcon } from "lucide-react";
 import { Footer } from "./components/Footer";
+
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
   return (
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,25 +1,81 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 
 import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(event: React.MouseEvent) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ir para a Home"
+        title="Ir para a home"
+      >
         <HouseIcon />
       </a>
 
-      <div className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ver Histórico"
+        title="Ver Histórico"
+      >
         <HistoryIcon />
-      </div>
+      </a>
 
-      <div className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ir para as Configurações"
+        title="Ir para as Configurações"
+      >
         <SettingsIcon />
-      </div>
+      </a>
 
-      <div className={styles.menuLink}>
-        <SunIcon />
-      </div>
+      <a
+        className={styles.menuLink}
+        aria-label="Mudar Tema da Página"
+        title="Mudar Tema da Página"
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
+      </a>
     </nav>
   );
 }
