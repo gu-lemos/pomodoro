1efbf8acd3088a6eec9dd3e12575e32e1e0e5cc2
Gustavo Lemos <contato.gustavolemos@outlook.com>
2025-12-17 00:21:42 -0300

    feat: main template, aboutPomodoro e NotFound

---
+++ src/templates/MainTemplate/index.tsx
@@ -0,0 +1,28 @@
+import { Footer } from "../../components/Footer";
+import { Logo } from "../../components/Logo";
+import { Container } from "../../components/Container";
+import { Menu } from "../../components/Menu";
+
+type MainTemplateProps = {
+  children: React.ReactNode;
+};
+
+export function MainTemplate({ children }: MainTemplateProps) {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      {children}
+
+      <Container>
+        <Footer />
+      </Container>
+    </>
+  );
+}
